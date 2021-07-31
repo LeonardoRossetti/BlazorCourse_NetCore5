1fb10776b2943deed99e2e8e9f53fa420647cf54
Leonardo Rossetti <leoo.rossetti@gmail.com>
2021-07-31 19:52:23 +1200

    Added generic repositories for the domains.

---
+++ CarRentalManagement/CarRentalManagement/Server/IRepository/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using CarRentalManagement.Shared.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalManagement.Server.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task Save();
+        IGenericRepository<Make> MakesRepository { get; }
+        IGenericRepository<Model> ModelRepository { get; }
+        IGenericRepository<Vehicle> VehicleRepository { get; }
+        IGenericRepository<Booking> BookingRepository { get; }
+        IGenericRepository<Colour> ColourRepository { get; }
+        IGenericRepository<Customer> CustomerRepository { get; }
+    }
+}
---
+++ CarRentalManagement/CarRentalManagement/Server/Repository/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using CarRentalManagement.Server.Data;
+using CarRentalManagement.Server.IRepository;
+using CarRentalManagement.Shared.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalManagement.Server.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        private IGenericRepository<Make> _makesRepository;
+        private IGenericRepository<Model> _modelRepository;
+        private IGenericRepository<Vehicle> _vehicleRepository;
+        private IGenericRepository<Booking> _bookingRepository;
+        private IGenericRepository<Colour> _colourRepository;
+        private IGenericRepository<Customer> _customerRepository;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IGenericRepository<Make> MakesRepository => _makesRepository ?? new GenericRepository<Make>(_context);
+        public IGenericRepository<Model> ModelRepository => _modelRepository ?? new GenericRepository<Model>(_context);
+        public IGenericRepository<Vehicle> VehicleRepository => _vehicleRepository ?? new GenericRepository<Vehicle>(_context);
+        public IGenericRepository<Booking> BookingRepository => _bookingRepository ?? new GenericRepository<Booking>(_context);
+        public IGenericRepository<Colour> ColourRepository => _colourRepository ?? new GenericRepository<Colour>(_context);
+        public IGenericRepository<Customer> CustomerRepository => _customerRepository ?? new GenericRepository<Customer>(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this); // to remove it from memory after it's used
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
